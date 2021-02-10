06b619f719545e4e3a8a72be21bd3effdeb67da5
Sean Wilken <sean.d.wilken@gmail.com>
2021-02-10 17:23:39 -0500

    Change contact cards style and content size

--- src/Client/Modules/Contact/Index.fs
+++ src/Client/Modules/Contact/Index.fs
@@ -16,46 +16,66 @@ let view =
         Level.level [] [
             Columns.columns [] [
                 Column.column [] [
-                    Container.container [ Container.Props [ ClassName "contactContentCard" ] ] [
-                        div [ ClassName "contentCardTextBackground" ] [
-                            h1 [] [ str "Sean Wilken" ]
-                            p [] [ str "Drop me a line with your questions or comments about the website or it's features. Feel free to also send a friendly hello. Just don't spam me." ]
-                            a [ Href "mailto: sean.d.wilken@gmail.com" ] [
-                                Level.level [] [
-                                    Level.item [] [ 
-                                        Image.image [ Image.Is64x64 ] [ img [ Src "./imgs/icons/Mail.png" ] ] 
-                                        div [] [ str "Sean.D.Wilken@GMail.com" ]
+                    Tile.ancestor [ Tile.Props [ ClassName "contactContentCard" ] ] [
+                        Tile.parent [] [
+                            Tile.child [ Tile.Size Tile.Is5 ] [
+                                Column.column [] [ Image.image [] [ img [ Src "./imgs/Misfortune.png" ] ] ]
+                            ]
+                            Tile.child [ Tile.Size Tile.Is7 ] [
+                                div [ ClassName "contentCardTextBackground" ] [
+                                    Columns.columns [] [
+                                        Column.column [] [ h1 [] [ str "Sean Wilken" ] ]
+                                        Column.column [] [ 
+                                            p [] [ str """Say hello or drop me a line with your questions or comments
+                                                         about the website and it's features. Or for any other reason 
+                                                         there might be... Just don't spam me.""" ]                                  
+                                        ]
+                                        Column.column [] [
+                                            a [ Href "mailto: sean.d.wilken@gmail.com" ] [
+                                                Level.level [] [ Level.item [] [ 
+                                                    Image.image [ Image.Is64x64 ] [ img [ Src "./imgs/icons/Mail.png" ] ] 
+                                                    div [] [ str "Sean.D.Wilken@GMail.com" ]
+                                                ] ]
+                                            ]
+                                        ]   
                                     ]
                                 ]
                             ]
-                            Column.column [] [ Container.container [] [ Image.image [] [ img [ Src "./imgs/Misfortune.png" ] ] ] ]
                         ]
                     ]
                 ]
-                Column.column [] [ 
-                    Container.container [ Container.Props [ ClassName "contactContentCard" ] ] [
-                        div [ClassName "contentCardTextBackground" ] [
-                            h1 [] [ str "Xero Effort" ] 
-                            p [] [ str "Coming very soon, to platforms near you." ]
-                            // p [] [ str "Check out Xero Effort & remember:" ]
-                            // p [] [ str "Follow, Like & Share" ]
-                            // a [ Href "https://www.instagram.com/xeroeffort/" ] [ 
-                            //     Level.level [] [
-                            //         Level.item [] [
-                            //             Image.image [ Image.Is64x64 ] [ img [ Src "./imgs/icons/IG.png" ] ]
-                            //             div [] [ str "@XeroEffort" ]
-                            //         ]
-                            //     ]
-                            // ]
-                            a [ Href "mailto: xeroeffortclub@gmail.com" ] [ 
-                                Level.level [] [
-                                    Level.item [] [
-                                        Image.image [ Image.Is64x64 ] [ img [ Src "./imgs/icons/Mail.png" ] ]
-                                        div [] [ str "XeroEffortClub@GMail.com" ] 
+                Column.column [] [
+                    Tile.ancestor [ Tile.Props [ ClassName "contactContentCard" ] ] [
+                        Tile.parent [] [
+                            Tile.child [ Tile.Size Tile.Is7 ] [
+                                div [ ClassName "contentCardTextBackground" ] [
+                                    Columns.columns [] [
+                                        Column.column [] [ h1 [] [ str "Xero Effort" ] ]
+                                        Column.column [] [ p [] [ str "Let's hear what you got to say about the drawings..." ] ]
+                                        Column.column [] [ a [ Href "mailto: xeroeffortclub@gmail.com" ] [ 
+                                            Level.level [] [
+                                                Level.item [] [
+                                                    Image.image [ Image.Is64x64 ] [ img [ Src "./imgs/icons/Mail.png" ] ]
+                                                    div [] [ str "XeroEffortClub@GMail.com" ] 
+                                                ]
+                                            ]
+                                        ] ]
+                                        Column.column [] [ p [] [ str "Coming very soon..." ] ]
+                                        // Column.column [] [ p [] [ str "Follow, Like & Share - Xero Effort" ] ]
+                                        // Column.column [] [ a [ Href "https://www.instagram.com/xeroeffort/" ] [ 
+                                        //     Level.level [] [
+                                        //         Level.item [] [
+                                        //             Image.image [ Image.Is64x64 ] [ img [ Src "./imgs/icons/IG.png" ] ]
+                                        //             div [] [ str "@XeroEffort" ]
+                                        //         ]
+                                        //     ]
+                                        // ] ]
                                     ]
                                 ]
                             ]
-                            Column.column [] [ Container.container [] [ Image.image [] [ img [ Src "./imgs/Out for Blood.png" ] ] ] ]
+                            Tile.child [ Tile.Size Tile.Is5 ] [
+                                Column.column [] [ Image.image [] [ img [ Src "./imgs/Out for Blood.png" ] ] ]
+                            ]
                         ]
                     ]
                 ]
